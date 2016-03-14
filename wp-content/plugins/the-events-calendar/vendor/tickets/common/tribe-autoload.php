4c14c4746df255bd63cac7499f996c2e1bd1f385
thetouristt <Mon@MacBook-Air-de-Mon.local>
2016-03-14 18:59:45 +0100

    Ajustes plugin + gailutheme
    Quedaban cambios por commit, eliminación del plugin de Eventos…
    
    Creo el childtheme, y archivo style.css

--- wp-content/plugins/the-events-calendar/vendor/tickets/common/tribe-autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-$src = dirname( __FILE__ ) . '/src';
-require_once $src . '/Tribe/Autoloader.php';
-
-$autoloader = Tribe__Autoloader::instance();
-$autoloader->register_prefix('Tribe__',$src);
-$autoloader->register_autoloader();
